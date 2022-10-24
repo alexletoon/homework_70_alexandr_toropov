82b1e3665f6930c1d907f558213c872e07821e3a
Alexandr Toropov <bligs100@gmail.com>
2022-10-24 21:05:15 +0600

    в файл accounts/forms.py добавлена форма создания нового пользователя,
    переопределены методы clean() и save()

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,7 +1,35 @@
 from django import forms
-
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class LoginForm(forms.Form):
     username = forms.CharField(required=True, label='Логин')
     password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
     next = forms.CharField(required=False, widget=forms.HiddenInput)
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label='Подтвердите Пароль', strip=False, required=True, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm']
+
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('cleaned_data.get')
+        if password and password_confirm and password != password_confirm:
+            raise ValidationError('Пароли не совпадают')
+
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password'))
+        if commit:
+            user.save()
+        return user
+
+

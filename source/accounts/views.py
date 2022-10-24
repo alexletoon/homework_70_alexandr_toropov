a52d2216af2a7553b46a720a85f228f297c8809f
Alexandr Toropov <bligs100@gmail.com>
2022-10-24 21:06:23 +0600

    в accounts/views.py добавлено классовое представление для создания нового пользователя при регистрации

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView
-from accounts.forms import LoginForm
+from django.views.generic import TemplateView, CreateView
+from accounts.forms import CustomUserCreationForm, LoginForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 
@@ -36,3 +36,20 @@ class LoginView(TemplateView):
 def logout_view(request):
     logout(request)
     return redirect('index_view')
+
+
+class RegisterView(CreateView):
+    template_name: str = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index_view')
+        context = {}
+        context['form'] = form 
+        return self.render_to_response(context)
